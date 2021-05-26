6f93ee811aa872ed523810cf5d61afd92de3a89c
msabat98 <mariasabatini-@hotmail.co.uk>
2021-05-26 13:58:39 +0100

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(celsius) {
+    return celsius * (9/5) + 32;
+}
+ 
+const celsiusInput = process.argv[2]; // Get the 3rd input from the argument list
+const fahrenheitValue = celsiusToFahrenheit(celsiusInput);
+ 
+console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue} degrees Fahrenheit`);
